adc29e3ac56b693cd35449e52aadd3ea6af1537a
Mateusz Bednarski <m@mdraco.com>
2014-11-08 13:13:01 +0100

    done

--- src/main/java/pl/allegro/devfest/BestDealAdvisor.java
+++ src/main/java/pl/allegro/devfest/BestDealAdvisor.java
@@ -3,6 +3,9 @@ package pl.allegro.devfest;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class BestDealAdvisor {
 
@@ -13,6 +16,33 @@ public class BestDealAdvisor {
     }
 
     public Optional<BestDeal> findBestDeal(DealCriteria dealRequirements) {
-        throw new UnsupportedOperationException("Implement me!");
+        Collection<Optional<Auction>> auctionsForRequirements =
+           dealRequirements
+          .getGoodTypes()
+          .stream()
+          .map(goodType -> findCheapestAuction(goodType))
+          .collect(Collectors.toList());
+
+        if (auctionsForRequirements.stream().anyMatch(optional -> !optional.isPresent()))
+          return Optional.empty();
+
+        Collection<Auction> auctions = auctionsForRequirements
+          .stream()
+          .map(optional -> optional.get())
+          .collect(Collectors.toList());
+
+        BestDeal bestDeal = new BestDeal(auctions);
+
+        return bestDeal.getTotalPrice().compareTo(dealRequirements.getBudget()) > 0
+          ? Optional.empty()
+          : Optional.of(bestDeal);
+    }
+
+    private Optional<Auction> findCheapestAuction(GoodType goodType) {
+        return auctions
+          .stream()
+          .filter(auction -> auction.getGoodType() == goodType)
+          .sorted((a1, a2) -> a1.getPrice().compareTo(a2.getPrice()))
+          .findFirst();
     }
 }
